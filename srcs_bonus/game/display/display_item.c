546b618b19bd06e040485950bdc22634a3f8d53d
276izo276 <izo276.discord@gmail.com>
2025-09-29 09:13:32 +0200

    fix: startfix enemy go to player

--- srcs_bonus/game/display/display_item.c
+++ srcs_bonus/game/display/display_item.c
@@ -263,18 +263,18 @@ void	display_item(t_data *data, int i)
 			check_dir_enemy(data, i, j);
 			data->ray[i].img_addr = data->screen->data_addr + data->ray[i].pix_x
 				* data->ray[i].calc_bits;
-			printf("deg >>%lf    %lf\n",data->ray[i].deg,data->ray[i].enemys[j]->enemy->deg);
+			// printf("deg >>%lf    %lf\n",data->ray[i].deg,data->ray[i].enemys[j]->enemy->deg);
 			// printf("ray >>%lf      >= >>%lf      <= >>%lf\n",fmod(data->ray[i].enemys[j]->enemy->deg, 360) + 360,fmod(data->ray[i].deg - 90, 360) + 360,fmod(data->ray[i].deg + 90, 360) + 360);
 			if (fmod(data->ray[i].enemys[j]->enemy->deg, 360) + 360 >= fmod(data->ray[i].deg, 360) - 90 + 360
 				&& fmod(data->ray[i].enemys[j]->enemy->deg, 360) + 360 <= fmod(data->ray[i].deg, 360) + 90 + 360)
 			{
-				printf("BACK\n");
+				// printf("BACK\n");
 				text_x = data->ray[i].enemys[j]->texture_coo.x
 					* (data->map.dementor_back->bits_per_pixel >> 3);
 			}
 			else
 			{
-				printf("FRONT\n");
+				// printf("FRONT\n");
 				text_x = data->ray[i].enemys[j]->texture_coo.x
 					* (data->map.dementor_front->bits_per_pixel >> 3);
 			}
