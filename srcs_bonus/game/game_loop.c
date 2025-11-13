0b6395718b3a2404dae106cd9d3d45901bd2084b
Antoine Le Guelvoud <izo276.discord@gmail.com>
2025-11-13 03:47:42 +0100

    modif spell and imgs

--- srcs_bonus/game/game_loop.c
+++ srcs_bonus/game/game_loop.c
@@ -80,14 +80,14 @@ static void	handle_input_move(t_data *data, long long int cur)
 				data->popo[3].call(data);
 			else if (data->keycode[i] == KEY_5)
 			{
-				data->item = add_end_lst(create_item(data, TIG, 
+				data->item = add_end_lst(create_item(data, MAC, 
 					&data->player.coo, data->map.mini.deg), data->item, f_item);
 				make_move_item(data->item->dt, 10);
 				data->keycode[i] = 0;
 			}
 			else if (data->keycode[i] == KEY_6)
 			{
-				data->item = add_end_lst(create_item(data, BH, 
+				data->item = add_end_lst(create_item(data, LOGO_42, 
 					&data->player.coo, data->map.mini.deg), data->item, f_item);
 				make_move_item(data->item->dt, 10);
 				data->keycode[i] = 0;
